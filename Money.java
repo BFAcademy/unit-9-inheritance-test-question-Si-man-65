9c2dc7ee117b453ff373f50f7d782de09c4e32b9
Si-man-65 <sheyman25@bfacademy.de>
2025-04-17 13:51:14 +0000

    Final

--- Money.java
+++ Money.java
@@ -1,4 +1,86 @@
 public class Money
 {
-  
+    private double val;
+
+    public Money(double v)
+    {
+       val = v;
+    }
+
+    public double getAmount()
+    {
+        return val;
+    }
+}
+
+class Coin extends Money
+{
+    private String name;
+    public Coin(String n, double v)
+    {
+        super(v);
+        name = n;
+    }
+    public String toString()
+    {
+        return name;
+    }
+    public String getName()
+    {
+        return name;
+    }
+
+    public boolean equals(Coin a)
+    {
+        if(a == null)
+        {
+            return false;
+        }
+        else if(this.getName().equals(a.getName()) && this.getAmount() == a.getAmount())
+        {
+            return true;
+        }
+        
+            return false;
+        
+    }
+
+}
+
+//Coin classes for different coins (nickel,dime, etc)
+
+class Quarter extends Coin
+{
+    public Quarter()
+    {
+        super("quarter", 0.25);
+    }
+}
+class Dime extends Coin
+{
+    public Dime()
+    {
+        super("dime", 0.10);
+    }
+}
+class Nickel extends Coin
+{
+    public Nickel()
+    {
+        super("nickel", 0.05);
+    }
+}
+
+class Bill extends Money
+{
+    public Bill(double v)
+    {
+        super(v);
+    }
+
+    public String toString()
+    {
+        return "$" + this.getAmount();
+    }
+
 }
